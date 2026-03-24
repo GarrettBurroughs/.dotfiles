c79f38b4baf7088b76fcf8bca138f16b917eb940
Garrett Burroughs <garrettburroughs02@gmail.com>
2026-03-24 09:26:39 -0400

    Remove install script

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # 1. Install Nix in single-user mode (no daemon required)
-sh <(curl -L https://nixos.org/nix/install) --no-daemon
+# sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
 # 2. Source the Nix environment so the current script can use the 'nix' command
-. ~/.nix-profile/etc/profile.d/nix.sh
+# . ~/.nix-profile/etc/profile.d/nix.sh
 
 # 3. Enable flakes temporarily for the install command
-mkdir -p ~/.config/nix
-echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
+# mkdir -p ~/.config/nix
+# echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
 
 # 4. Run your Home Manager flake
-nix run github:nix-community/home-manager -- switch --flake .#$USER -b backup
+# nix run github:nix-community/home-manager -- switch --flake .#$USER -b backup
